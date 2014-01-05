c31348649421c085c2c0c6013d3ca57fa9718e0e
Themaister <maister@archlinux.us>
2014-01-05 14:18:59 +0100

    Tweak 3phase filter responses.

--- ntsc/filter_3phase.m
+++ ntsc/filter_3phase.m
@@ -1,24 +1,30 @@
-T = 4.1;
-beta = 0.45;
-tt = -24 : 24;
+T = 3.9;
+beta = 0.50;
+tt = -22.5 : 22.5;
 
 % Cosine windows reduce ringing artifacts.
 cosine_window = cos(pi * beta * tt / T) ./ (1 - (2 * beta * tt).^2 / (T * T));
+luma = (1 / T) * sinc(tt / T) .* cosine_window .* kaiser(length(tt), 4.0)';
+luma = conv(luma, [1 0 0 1] / 2);
 
-luma = (1 / T) * sinc(tt / T) .* cosine_window .* kaiser(49, 1.0)';
+T = 12.2;
+beta = 0.50;
+tt = -22.5 : 22.5;
 
-chroma = gausswin(49);
-chroma = chroma / sum(chroma);
+% Cosine windows reduce ringing artifacts.
+cosine_window = cos(pi * beta * tt / T) ./ (1 - (2 * beta * tt).^2 / (T * T));
+chroma = (1 / T) * sinc(tt / T) .* cosine_window .* kaiser(length(tt), 4.0)';
+chroma = conv(chroma, [1 0 0 1] / 2);
 
 close all;
 figure(1);
-freqz(luma);
+freqz(luma, 1, 16 * 1024);
 
 figure(2);
 stem(luma);
 
 figure(3);
-freqz(chroma);
+freqz(chroma, 1, 16 * 1024);
 
 file = fopen('ntsc-decode-filter-3phase.inc', 'w');
 fprintf(file, '#define TAPS 24\n');
