84213a7f0678d3ce382f99c0fac6b89910a3f9f7
hunterk <HKaller@CLEM215-HK-WIN7.dhcp.tntech.edu>
2014-02-21 08:22:00 -0600

    split AMD/Ati fix into separate shaders+cgp to avoid the need for end-users to manually edit

--- crt/gtu-v050/config.h
+++ crt/gtu-v050/config.h
@@ -1,5 +1,5 @@
 
- #define compositeConnection
+#define compositeConnection
 // #define noScanlines
 
 #define signalResolution		256.0
@@ -10,6 +10,4 @@
 #define blackLevel 0.0875
 #define contrast 1.0
 
-// uncomment the following line to fix the shader on AMD/Ati GPUs
-//#define ATIFIX
-#define FIXNUM 6 // this should be a value between 2 and 12. Lower values will have less vibrant color when 'compositeConnection' is enabled, but higher values will be slower.
+#define FIXNUM 6
