be70c17f868e55d5a3e4b16a634ac2813a1539fe
Monroe88 <wmsmart88@gmail.com>
2016-02-02 19:22:53 -0600

    (GTU) Indentation nits

--- crt/shaders/gtu-v050/config.h
+++ crt/shaders/gtu-v050/config.h
@@ -8,13 +8,13 @@ uniform float compositeConnection;
 #define compositeConnection   0.0
 #define noScanlines           0.0
 
-#define signalResolution		256.0
-#define signalResolutionI		83.0
-#define signalResolutionQ		25.0
-#define tvVerticalResolution	250.0
+#define signalResolution      256.0
+#define signalResolutionI     83.0
+#define signalResolutionQ     25.0
+#define tvVerticalResolution  250.0
 
-#define blackLevel 0.0875
-#define contrast 1.0
+#define blackLevel            0.0875
+#define contrast              1.0
 #endif
 
 #define FIXNUM 6
